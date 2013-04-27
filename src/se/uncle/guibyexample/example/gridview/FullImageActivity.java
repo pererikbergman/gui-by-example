ea6f13851bbbe881ae29072f318ae5f2534b4871
Per-Erik Bergman <bergman@uncle.se>
2013-04-27 11:38:26 +0200

    Change to folow the android standard.

--- src/se/uncle/guibyexample/example/gridview/FullImageActivity.java
+++ src/se/uncle/guibyexample/example/gridview/FullImageActivity.java
@@ -1,35 +1,31 @@
 package se.uncle.guibyexample.example.gridview;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import se.uncle.guibyexample.R;
+import se.uncle.guibyexample.example.gridview.util.SystemUiHider;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class FullImageActivity extends Activity {
 
+	private static final boolean AUTO_HIDE = true;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	private static final boolean TOGGLE_ON_CLICK = true;
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
 	public static final String POSITION = "position";
+
+	private SystemUiHider mSystemUiHider;
 
 	private ViewPager mPager;
 	private PagerAdapter mPagerAdapter;
-	private View mTopbar;
-	private View mBottombar;
-	private boolean mGUIIsShowing = false;
 
-	private Timer mTimer = new Timer();
-	private HideGUITimerTask mTimerTask;
-	
 	public int[] mThumbIds = { R.drawable.pic_1, R.drawable.pic_2,
 			R.drawable.pic_3, R.drawable.pic_4, R.drawable.pic_5,
 			R.drawable.pic_6, R.drawable.pic_7, R.drawable.pic_8,
@@ -42,137 +38,102 @@ public class FullImageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fullscreen);
 
+		// Get the starting position.
 		Bundle extras = getIntent().getExtras();
 		if (extras == null) {
 			return;
 		}
 		int position = extras.getInt(POSITION);
 
+		// Set up the pager view.
 		mPager = (ViewPager) findViewById(R.id.myfivepanelpager);
 		mPagerAdapter = new ViewPagerAdapter(this, mThumbIds);
 		mPager.setAdapter(mPagerAdapter);
 		mPager.setCurrentItem(position);
 
-		mPager.setOnTouchListener(new ResetGUITimer());
-		
+		// Setting up the 'back' button.
 		Button back = (Button) findViewById(R.id.back);
 		back.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				resetTimer();
 				int idx = (mPager.getCurrentItem() - 1) % mThumbIds.length;
 				mPager.setCurrentItem(idx);
 			}
 		});
+		back.setOnTouchListener(mDelayHideTouchListener);
 
+		// Setting up the 'next' button.
 		Button next = (Button) findViewById(R.id.next);
 		next.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				resetTimer();
 				int idx = (mPager.getCurrentItem() + 1) % mThumbIds.length;
 				mPager.setCurrentItem(idx);
 			}
 		});
+		next.setOnTouchListener(mDelayHideTouchListener);
 
+		// Setting up the 'home' button.
 		Button home = (Button) findViewById(R.id.home);
 		home.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				 FullImageActivity.this.finish();
+				FullImageActivity.this.finish();
 			}
 		});
+		home.setOnTouchListener(mDelayHideTouchListener);
 
-		mTopbar = (LinearLayout) findViewById(R.id.topbar);
-		mBottombar = (LinearLayout) findViewById(R.id.bottombar);
-		showGUI();
-		
-		startTimer();
+		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		mSystemUiHider = SystemUiHider.getInstance(this, mPager, HIDER_FLAGS);
+		mSystemUiHider.setup();
+		mSystemUiHider.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+					@Override
+					public void onVisibilityChange(boolean visible) {
+						controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+						if (visible && AUTO_HIDE) {
+							delayedHide(AUTO_HIDE_DELAY_MILLIS);
+						}
+					}
+				});
+
+		mPager.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (TOGGLE_ON_CLICK) {
+					mSystemUiHider.toggle();
+				} else {
+					mSystemUiHider.show();
+				}
+			}
+		});
 	}
 
-	/** Timer functions **/
-	
-	private void startTimer() {
-		mTimerTask = new HideGUITimerTask();
-		mTimer.schedule(mTimerTask, 3000);
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		delayedHide(100);
 	}
-	
-	private void resetTimer() {
-		mTimerTask.cancel();
-		startTimer();
-	}
-	
-	/** Show & Hide GUI **/
-	
-	private void showGUI() {
-		if ( mGUIIsShowing ){
-			return;
+
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
 		}
-		Animation fadeIn = new AlphaAnimation(0, 1);
-		fadeIn.setDuration(500);
-		mTopbar.startAnimation(fadeIn);
-		mTopbar.setVisibility(View.VISIBLE);
-		
-		fadeIn = new AlphaAnimation(0, 1);
-		fadeIn.setDuration(500);
-		mBottombar.startAnimation(fadeIn);
-		mBottombar.setVisibility(View.VISIBLE);
-		mGUIIsShowing = true;
-	}
+	};
 
-	private void hideGUI() {
-		if ( !mGUIIsShowing ){
-			return;
-		}
-		Animation fadeOut = new AlphaAnimation(1, 0);
-		fadeOut.setDuration(500);
-		mTopbar.startAnimation(fadeOut);
-		mTopbar.setVisibility(View.INVISIBLE);
-		
-		mBottombar.startAnimation(fadeOut);
-		mBottombar.setVisibility(View.INVISIBLE);
-		mGUIIsShowing = false;
-	}
-	
-	/** HELP CLASSES **/
-	
-	private class HideGUITimerTask extends TimerTask {
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
 		@Override
 		public void run() {
-			runOnUiThread(new Runnable() {
-				
-				@Override
-				public void run() {
-					hideGUI();
-				}
-			});
-			
+			mSystemUiHider.hide();
 		}
-	}
-	
-	private class ResetGUITimer implements OnTouchListener {
-		private static final float THRESHOLD = 5f;
-		
-	    private float mLastX = 0;
+	};
 
-	    @Override
-	    public boolean onTouch(View v, MotionEvent event) {
-	        switch (event.getAction()) {
-	        case MotionEvent.ACTION_DOWN:
-	            mLastX = event.getX();
-	            break;
-
-	        case MotionEvent.ACTION_UP:
-	            float newX = event.getX();
-	            if (Math.abs(mLastX - newX) < THRESHOLD) {
-	                showGUI();
-	                resetTimer();
-	                return true;
-	            }
-	            mLastX = 0;
-	            break;
-	        }
-	        return false;
-	    }
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
 }
