a0bd8278ad25e5345166966e8b46ab1c32496b44
Per-Erik Bergman <bergman@uncle.se>
2013-05-04 23:02:28 +0200

    Cleaned up the project.

--- src/se/uncle/guibyexample/ActivityListStarter.java
+++ src/se/uncle/guibyexample/ActivityListStarter.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -69,12 +68,5 @@ public class ActivityListStarter extends Activity {
 			return true;
 		}
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 }
